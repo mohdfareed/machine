1a4146f0591ba136da73ec8cbb5a1ca1976bb17f
Mohammed Fareed <mohdf.fareed@icloud.com>
2024-10-04 21:00:37 -0400

    refactor(testing.py): use partial to simplify private command setup fix(testing.py): move environment initialization inside conditional blocks
    The private command setup is simplified using functools.partial, which reduces redundancy and improves code readability. The environment variables win_env and unix_env are now initialized within their respective conditional blocks, ensuring they are only created when necessary, which optimizes resource usage and prevents potential errors in environments where they are not applicable.

--- app/machines/testing.py
+++ app/machines/testing.py
@@ -1,5 +1,6 @@
 """Testing machine."""
 
+from functools import partial
 from time import sleep
 
 import typer
@@ -7,14 +8,11 @@ from rich.progress import track
 
 from app import utils
 from app.config import MachineConfig, UnixEnvironment, WindowsEnvironment
-
-# from app.plugins import private_files
+from app.plugins import private_files
 
 app: typer.Typer = typer.Typer(name="test")
-
-config_files = MachineConfig()
-win_env = WindowsEnvironment()
-unix_env = UnixEnvironment()
+private = partial(private_files.setup)
+app.command(name="private")(private.func)
 
 
 @app.callback(invoke_without_command=True)
@@ -23,11 +21,15 @@ def setup(cx: typer.Context) -> None:
     if cx.invoked_subcommand is not None:
         return
 
+    config_files = MachineConfig()
     utils.LOGGER.debug("Config files: %s", config_files.model_dump_json(indent=2))
 
     if utils.WINDOWS:
+        win_env = WindowsEnvironment()
         utils.LOGGER.debug("Environment: %s", win_env.model_dump_json(indent=2))
-    else:
+
+    if utils.LINUX or utils.MACOS:
+        unix_env = UnixEnvironment()
         utils.LOGGER.debug("Environment: %s", unix_env.model_dump_json(indent=2))
 
     utils.LOGGER.info("Setting up machine...")
@@ -36,10 +38,3 @@ def setup(cx: typer.Context) -> None:
 
     utils.LOGGER.info("Machine setup completed successfully")
     raise typer.Exit()
-
-
-# @app.command()
-# def private() -> None:
-#     """Setup private files on a machine."""
-#     private_files.setup()
-#     raise typer.Exit()
