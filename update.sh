90685b4931ac2b157c9d2ff2ccf66b7fd31f3cab
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-09-19 20:37:32 -0400

    Modify update script for pre-commit hooks

--- update.sh
+++ update.sh
@@ -2,4 +2,9 @@
 set -euo pipefail
 
 echo "updating pre-commit hooks..."
-pre-commit autoupdate || true
+# FIXME: black and isort dropped support for Python 3.8
+# if [ -f .venv/bin/pre-commit ]; then
+#   "./.venv/bin/pre-commit" autoupdate || true
+# else
+#   pre-commit autoupdate || true
+# fi
