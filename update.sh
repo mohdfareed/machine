01b2c3712f93aed3b987e65a3f3ab7a0b362d7a5
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-09-19 21:28:47 -0400

    Compile python scripts during `update.sh` script

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "compiling scripts..."
+python -m compileall -qf chezmoi
+python -m compileall -qf config
+python -m compileall -qf machines
+python -m compileall -qf scripts
+python -m compileall -qf bootstrap.py
+
 echo "updating pre-commit hooks..."
 # FIXME: black and isort dropped support for Python 3.8
 # if [ -f .venv/bin/pre-commit ]; then
