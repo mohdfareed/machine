d0e72f8c2c698bed9ad0451392d77b2c7d5a413b
Mohammed Fareed <mohdf.fareed@icloud.com>
2026-02-15 02:35:10 -0500

    Install `uv` to temp dir during bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,14 +3,13 @@ $ErrorActionPreference = "Stop"
 # Bootstrap: irm https://raw.githubusercontent.com/mohdfareed/machine/main/bootstrap.ps1 | iex
 $Root = if ($env:MC_ROOT) { $env:MC_ROOT } else { "$HOME\.machine" }
 
-# ensure git and uv are available
-winget install "git.git" "astral-sh.uv"
-
 # clone repo if needed
+winget install "git.git"
 if (-not (Test-Path "$Root\.git")) {
     git clone https://github.com/mohdfareed/machine.git "$Root"
 }
 
 # install the cli tool
+winget install "astral-sh.uv"
 uv tool install $Root --force
 Write-Host "Done. Run 'mc --help' to get started."
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@ set -eu
 # Bootstrap: curl -LsSf https://raw.githubusercontent.com/mohdfareed/machine/main/bootstrap.sh | sh
 ROOT="${MC_ROOT:-$HOME/.machine}"
 UV="uv"
+TMP_DIR=""
 
-# ensure git is available
-if ! command -v git >/dev/null 2>&1; then
-    echo "Error: git is not installed. Please install git and try again."
-    exit 1
+# clone repo if needed
+if ! [ -d "$ROOT/.git" ]; then
+    git clone https://github.com/mohdfareed/machine.git "$ROOT"
 fi
 
 # ensure uv is available
@@ -18,16 +18,14 @@ if ! command -v uv >/dev/null 2>&1; then
     export UV_NO_MODIFY_PATH=1
     export UV_DISABLE_UPDATE=1
 
+    # install uv to temp dir
     curl -LsSf https://astral.sh/uv/install.sh | sh
     UV="$TMP_DIR/uv"
-fi
-
-# clone repo if needed
-if ! [ -d "$ROOT/.git" ]; then
-    git clone https://github.com/mohdfareed/machine.git "$ROOT"
+    bin="$("$UV" tool dir --bin)"
+    export PATH="$bin:$PATH"
 fi
 
 # install the cli tool
 "$UV" tool install "$ROOT" --force
-rm -rf "$TMP_DIR"
+[ -n "$TMP_DIR" ] && rm -rf "$TMP_DIR" # clean up
 echo "Done. Run 'mc --help' to get started."
