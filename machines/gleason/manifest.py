433378e7f236e5f0cf70318341fe635dca40f3f3
Mohammed Fareed <mfareed@gleason.com>
2026-03-31 14:48:25 -0400

    Reorder and remove packages in gleason manifest

--- machines/gleason/manifest.py
+++ machines/gleason/manifest.py
@@ -10,11 +10,10 @@ manifest = MachineManifest(
         Package(name="power-toys", winget="microsoft.powertoys"),
         Package(name="sys-internals", winget="microsoft.sysinternals.suite"),
         Package(name="advanced-system-settings", winget="9N8MHTPHNGVV"),
-        Package(name="dev-home", winget="microsoft.DevHome"),
-        Package(name="go", winget="golang.Go"),
         Package(name="docker", winget="docker.DockerDesktop"),
         Package(name="jetbrains-mono", winget="DEVCOM.JetBrainsMonoNerdFont"),
         Package(name="chatgpt", winget="9NT1R1C2HH7J"),
         Package(name="craft-docs", winget="LukiLabs.Craft"),
+        Package(name="go", winget="golang.Go"),
     ],
 )
